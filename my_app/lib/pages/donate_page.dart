5e7d91e5f61d202a40f73a3de93fe987ad761ca6
abarambulo <abarambulo1@up.edu.ph>
2024-05-25 19:01:14 +0800

    added: more fields in donate page

--- my_app/lib/pages/donate_page.dart
+++ my_app/lib/pages/donate_page.dart
@@ -4,9 +4,20 @@ import 'package:provider/provider.dart';
 import 'signin_page.dart';
 import '../providers/auth_provider.dart';
 
-class DonatePage extends StatelessWidget {
+class DonatePage extends StatefulWidget {
+  const DonatePage({super.key});
+
+  @override
+  State<DonatePage> createState() => _DonatePageState();
+}
+
+class _DonatePageState extends State<DonatePage> {
 
   bool? foodCheckboxValue = false;
+  bool? clothesCheckboxValue = false;
+  bool? cashCheckboxValue = false;
+  bool? necessitiesCheckboxValue = false;
+  String? _dropdownValue = 'pickup';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,11 +51,51 @@ class DonatePage extends StatelessWidget {
               title: const Text("Food"),
               value: foodCheckboxValue,
               onChanged: (bool? value) {
-                // setState(() {
-                //   foodCheckboxValue = value;
-                // })
+                setState(() {
+                  foodCheckboxValue = value;
+                });
               }
             ),
+            CheckboxListTile(
+              title: const Text("Clothes"),
+              value: clothesCheckboxValue,
+              onChanged: (bool? value) {
+                setState(() {
+                  clothesCheckboxValue = value;
+                });
+              }
+            ),
+            CheckboxListTile(
+              title: const Text("Cash"),
+              value: cashCheckboxValue,
+              onChanged: (bool? value) {
+                setState(() {
+                  cashCheckboxValue = value;
+                });
+              }
+            ),
+            CheckboxListTile(
+              title: const Text("Necessities"),
+              value: necessitiesCheckboxValue,
+              onChanged: (bool? value) {
+                setState(() {
+                  necessitiesCheckboxValue = value;
+                });
+              }
+            ),
+            DropdownButton(
+              items: const [
+                DropdownMenuItem(child: Text("Pick-up"), value: "pickup"),
+                DropdownMenuItem(child: Text("Drop-off"), value: "dropoff"),
+              ],
+              
+              value: _dropdownValue,
+              onChanged: (String? value) {
+                setState(() {
+                  _dropdownValue = value;
+                });
+              }
+            ), 
             SizedBox(height: 20),
              // Widget to display donors
           ],
